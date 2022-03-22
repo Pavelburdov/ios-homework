165778b3f095eec7d306f79ba2689e427ec9f934
PavelBurdov <roqez@yandex.ru>
2022-03-22 12:49:25 +0300

    добавляем новую группу Profile

---
+++ Profile/ProfileHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Pavel on 22.03.2022.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
