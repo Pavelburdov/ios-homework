8df20553a2cb025fa95af0faa0ba57bab15e0607
PavelBurdov <roqez@yandex.ru>
2022-03-22 17:54:14 +0300

    Добавлено аватарка, подпись, статус и кнопка

--- Profile/ProfileHeaderView.swift
+++ Profile/ProfileHeaderView.swift
@@ -9,12 +9,63 @@ import UIKit
 
 class ProfileHeaderView: UIView {
 
-    /*
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Cat")
+        imageView.frame = CGRect(x: 16, y: 16, width: 150, height: 150)
+        imageView.layer.borderWidth = 3.0
+        imageView.layer.masksToBounds = false
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.cornerRadius = 75
+        imageView.contentMode = .scaleToFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = true
+        return imageView
+    }()
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Cat"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 17.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var statusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hunting"
+        label.textColor = .gray
+        label.font = UIFont.systemFont(ofSize: 14.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var myButton: UIButton = {
+        let button = UIButton()
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 4.0
+        button.backgroundColor = .systemBlue
+        button.setTitle("Show status", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.shadowOpacity = 0.7
+        button.addTarget(self, action: #selector(didTapMyButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
 
+
+
+    }
+    
+
+    private func drawSelf() {
+        self.addSubview(nameLabel)
+    }
 }
