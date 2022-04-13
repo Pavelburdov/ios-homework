99e86c78087da31742622f77d6b5e79dc16fdb52
PavelBurdov <roqez@yandex.ru>
2022-04-13 18:19:37 +0300

    added photos.swift files

--- Models/Photo.swift
+++ Models/Photo.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+struct Cars {
+
+    var image: String
+}
--- Navigation/CarsPhoto.swift
+++ Navigation/CarsPhoto.swift
@@ -6,4 +6,26 @@
 //
 
 import Foundation
-let carImage: []
+
+let carImage: [Cars] = [
+Cars(image: "img1"),
+Cars(image: "img2"),
+Cars(image: "img3"),
+Cars(image: "img4"),
+Cars(image: "img5"),
+Cars(image: "img6"),
+Cars(image: "img7"),
+Cars(image: "img8"),
+Cars(image: "img9"),
+Cars(image: "img10"),
+Cars(image: "img11"),
+Cars(image: "img12"),
+Cars(image: "img13"),
+Cars(image: "img14"),
+Cars(image: "img15"),
+Cars(image: "img16"),
+Cars(image: "img17"),
+Cars(image: "img18"),
+Cars(image: "img19"),
+Cars(image: "img20")
+]
